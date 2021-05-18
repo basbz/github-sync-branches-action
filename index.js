6edf52fafda076e54db0285dd8253d2a2e89ea1b
Bas Ben Zineb <bas.benzineb@gmail.com>
2021-05-18 12:10:16 +0200

    Use octokit to open PR

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 const core = require('@actions/core');
+const github = require("@actions/github");
 const wait = require('./wait');
 
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+     const {
+      payload: { repository }
+    } = github.context;
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+    const githubToken = core.getInput("GITHUB_TOKEN", { required: true });
+    const octokit = new github.GitHub(githubToken);
+    const fromBranch = "main"
+    const toBranch = "develop"
 
-    core.setOutput('time', new Date().toTimeString());
+     const { data: pullRequest } = await octokit.pulls.create({
+        owner: repository.owner.login,
+        repo: repository.name,
+        head: fromBranch ,
+        base: toBranch,
+        title: `sync: ${fromBranch} to ${toBranch}`,
+        body: `sync-branches: New code has just landed in ${fromBranch}, so let's bring ${toBranch} up to speed!`,
+      });
+
+
+    console.log(pullRequest)
   } catch (error) {
+
     core.setFailed(error.message);
   }
 }
